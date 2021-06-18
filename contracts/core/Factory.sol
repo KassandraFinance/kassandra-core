894b408c22d375112995ac8468ef910f418cf474
André Zanghelini <An-dz@users.noreply.github.com>
2021-06-18 16:13:55 -0300

    Use Ownable util on Core Factory

--- contracts/core/Factory.sol
+++ contracts/core/Factory.sol
@@ -6,14 +6,14 @@ pragma solidity ^0.8.0;
 
 import "./Pool.sol";
 
+import "../utils/Ownable.sol";
+
 /**
  * @title Pool Factory
  */
-contract Factory {
+contract Factory is Ownable {
     // map of all pools
     mapping(address=>bool) private _isPool;
-    // controller/admin address
-    address private _controller;
 
     /**
      * @dev Every new pool gets broadcast of its creation
@@ -38,24 +38,12 @@ contract Factory {
     );
 
     /**
-     * @dev Checks if call comes from current controller/admin
-     */
-    modifier onlyController() {
-        require(msg.sender == _controller, "ERR_NOT_CONTROLLER");
-        _;
-    }
-
-    constructor() {
-        _controller = msg.sender;
-    }
-
-    /**
      * @dev Create a new Pool
      *
      * @return Address of new Pool contract
      */
     function newPool()
-        external onlyController
+        external onlyOwner
         returns (Pool)
     {
         Pool pool = new Pool();
@@ -65,23 +53,11 @@ contract Factory {
         return pool;
     }
 
-    /**
-     * @dev Change the controller of this contract
-     *
-     * @param controller New controller address
-     */
-    function setController(address controller)
-        external onlyController
-    {
-        emit LogNewController(msg.sender, controller);
-        _controller = controller;
-    }
-
     function collect(Pool pool)
-        external onlyController
+        external onlyOwner
     {
         uint collected = IERC20(pool).balanceOf(address(this));
-        bool xfer = pool.transfer(_controller, collected);
+        bool xfer = pool.transfer(this.getController(), collected);
         require(xfer, "ERR_ERC20_FAILED");
     }
 
@@ -97,17 +73,5 @@ contract Factory {
         returns (bool)
     {
         return _isPool[b];
-    }
-
-    /**
-     * @dev Get address of who can create pools from this contract
-     *
-     * @return Address of the controller wallet or contract
-     */
-    function getController()
-        external view
-        returns (address)
-    {
-        return _controller;
     }
 }
